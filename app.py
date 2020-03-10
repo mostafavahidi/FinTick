391a4f74b8468ea6986bf3de49505bb7543e7fd1
Mostafa Vahidi <mostafa.vahidi@gmail.com>
2020-03-09 20:42:17 -0700

    Added suggestions list. Home page clean up.

--- app.py
+++ app.py
@@ -3,25 +3,18 @@ from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-	return render_template("analyze.html")
-
-@app.route("/admin")
-def admin():
-	return redirect(url_for("home"))
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
+@app.route("/", methods=["POST", "GET"])
+def analyze():
     if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+        tickerName = request.form["tickerName"]
+        return redirect(url_for("tickerCalc", ticker=tickerName))
     else:
-        return render_template("login.html")
+	    return render_template("analyze.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/<ticker>")
+def tickerCalc(ticker):
+    return f"<h1>{ticker}</h1>"
+
 
 
 if __name__ == "__main__":
